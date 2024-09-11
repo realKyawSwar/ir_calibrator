de111348c9e4857797bea0180f796d10ce36c1a5
Kyaw Swar <kyaw.s.thein@gmail.com>
2024-09-11 17:14:47 +0800

    Add files via upload

---
+++ modules/controller.py
@@ -0,0 +1,160 @@
+from time import sleep
+from modules.black_box import read_temp, set_temp
+from modules import ir_sensor
+# import ir_sensor
+import numpy as np
+import time
+
+class TemperatureController:
+    def __init__(self, com_port):
+        self.com_port = com_port
+
+    def set_temperature(self, temperature):
+        set_temp(self.com_port, temperature)
+
+    def get_temperature(self):
+        return read_temp(self.com_port)
+
+
+def timer_function(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"took {elapsed_time:.4f} seconds to complete.")
+        return result
+    return wrapper
+
+
+@timer_function
+def wait_until_temp_reached(controller, set_temp):
+    while True:
+        current_temp, _ = controller.get_temperature()
+        if current_temp == set_temp:
+            break
+        print(f"Waiting for current temperature to reach {set_temp}°C...")
+        sleep(60)
+
+
+def get_order(controller, listx=list(range(280, 345, 5))):
+    # Find the minimum and maximum values of the list
+    current_temp, _ = controller.get_temperature()
+    distance_to_min = abs(current_temp - min(listx))
+    distance_to_max = abs(current_temp - max(listx))
+    if distance_to_min < distance_to_max:
+        return listx
+    elif distance_to_max < distance_to_min:
+        return listx[::-1]
+    else:
+        return listx
+
+
+def get_slope(controller, bb_port, temp_port, set_port):
+    bb_temperature = controller.get_temperature()
+    print(f"Black box is {bb_temperature}")
+    listx = get_order(controller)
+    print(listx)
+    listy = []
+    for target_temp in listx:
+        print(f"Setting temperature to {target_temp}°C...")
+        controller.set_temperature(target_temp)
+        # Wait until current temperature matches set temperature
+        wait_until_temp_reached(controller, target_temp)
+        # Wait for 3 minutes
+        # print("Waiting for 1 minutes...")
+        # sleep(60)
+        ir_temperature = ir_sensor.read_temp(temp_port, 0x0100) / 10
+        print(ir_temperature)
+        listy.append(ir_temperature)
+    slope, intercept = np.polyfit(listx, listy, 1)
+    return slope, intercept
+
+
+def slope_routine(controller, bb_port, temp_port, set_port):
+    # Get the initial slope and intercept
+    slope, intercept = get_slope(controller, bb_port, temp_port, set_port)
+    dicty = {'initial': (slope, intercept)}
+    original_slope = 1
+    # Initialize span_input and old_slope
+    span_input = original_slope - (slope - 1)
+    tries = 1  # Initialize the tries counter
+    # Continue adjusting until slope is within range
+    while not 0.995 <= slope <= 1.005:
+        # Calculate the sensor correction value
+        sesp = round(span_input * 1000)
+        ir_sensor.write_correction(set_port, 0x021B, value=sesp)
+        # Update the slope and intercept after adjustment
+        slope, intercept = get_slope(controller, bb_port, temp_port, set_port)
+        # Log the new values in the dictionary
+        dicty[f'try_{tries}'] = (slope, intercept)
+        # Update span_input for the next iteration
+        span_input = (sesp/1000) - (slope - 1)
+        # Increment tries counter
+        tries += 1
+    print("Slope is now within the range:", slope)
+    print("All recorded slope and intercept values:", dicty)
+    return slope, intercept, dicty
+
+
+def to_signed(value: int):
+    if value >= 32768:
+        value -= 65536
+    return value/10
+
+
+def to_unsigned(value):
+    if value < 0:
+        value += 65536  # Convert negative to two's complement
+    return value
+
+
+def intercept_routine(controller, bb_port, temp_port, set_port, slope, intercept, dicty):
+    # Initialize span_input and old_slope
+    original_intercept = 0
+    offset_input =  original_intercept - intercept
+
+    tries = len(dicty)
+    # Continue adjusting until slope intercept is within range
+    while not (-1 <= intercept <= 1) and (0.995 <= slope <= 1.005):
+        if not -1 <= intercept <= 1:
+            # Calculate the sensor correction value
+            # seof = round(offset_input * 1000)
+            unsigned_offset = round(to_unsigned(offset_input))
+            ir_sensor.write_correction(set_port, 0x021C, value=unsigned_offset)
+
+        sleep(5)
+
+        if not 0.995 <= slope <= 1.005:
+            present_slope = ir_sensor.read_correction(set_port, 0x021B)
+            dummy_sp = present_slope / 1000
+            span_input = dummy_sp - (slope - 1)
+            sesp = round(span_input * 1000)
+            ir_sensor.write_correction(set_port, 0x021B, value=sesp)
+
+        # Update the slope and intercept after adjustment
+        slope, intercept = get_slope(controller, bb_port, temp_port, set_port)
+        dicty[f'try_{tries}'] = (slope, intercept)
+        # Update span_input for the next iteration
+        # check negative here!
+        seof = ir_sensor.read_correction(set_port, 0x021C)
+        signed_seof = to_signed(seof)
+        offset_input = signed_seof - intercept
+        # Increment tries counter
+        tries += 1
+    print("intercept is now within the range:", intercept)
+    print("All recorded slope and intercept values:", dicty)
+    return dicty
+
+
+if __name__ == "__main__":
+    bb_port = "COM14"
+    temp_port = "COM4"
+    set_port = "COM13"
+    controller = TemperatureController(bb_port)
+    # slopey, inty, dicty = slope_routine(controller, bb_port, temp_port, set_port)
+    # new_dicty = intercept_routine(controller, bb_port, temp_port, set_port, slopey, inty, dicty)
+    # print(new_dicty)
+    seof = ir_sensor.read_correction(set_port, 0x021C)
+    signed_value = to_signed(seof)
+    print(signed_value)
---
+++ modules/ir_sensor.py
@@ -0,0 +1,181 @@
+import modbus_tk.modbus_rtu as modbus_rtu
+import serial
+from modbus_tk import defines as cst
+from modbus_tk import modbus
+import time
+
+
+def connect(baud):
+    def decorator(func):
+        def wrapper(port, *args, **kwargs):
+            try:
+                # Configure the serial port
+                serial_port = serial.Serial(
+                    port=port,      # Use the port passed to the decorator
+                    baudrate=baud,  # Replace with your baud rate
+                    bytesize=8,
+                    parity='N',     # Parity: N=None, E=Even, O=Odd
+                    stopbits=1,
+                    timeout=1
+                )
+                client = modbus_rtu.RtuMaster(serial_port)
+                client.set_timeout(1.0)
+                client.set_verbose(True)
+                # Inject the client into the function
+                kwargs['client'] = client
+                # Call the decorated function
+                result = func(port, *args, **kwargs)
+            except modbus.ModbusError as e:
+                print(f"Modbus error: {e}")
+                result = None
+            except Exception as e:
+                print(f"Error: {e}")
+                result = None
+            finally:
+                # Close the Modbus connection
+                client.close()
+            return result
+        return wrapper
+    return decorator
+
+
+@connect(baud=19200)
+def read_temp(port, register=0x0100, client=None):
+    try:
+        # Read holding register (function code 03) from slave address 0x02
+        response = client.execute(0x02, cst.READ_HOLDING_REGISTERS, register, 1)
+        result = response[0]
+        # print(f"Temperature: {temperature}°C")
+        # print(f"result: {result}")
+    except modbus.ModbusError as e:
+        print(f"Modbus error: {e}")
+        result = None
+    except Exception as e:
+        print(f"Error: {e}")
+        result = None
+    return result
+
+
+@connect(baud=19200)
+def write_temp(port, register, value, client=None):
+    try:
+        # Write single holding register (function code 06) to slave address 0x01
+        client.execute(0x02, cst.WRITE_SINGLE_REGISTER, register, output_value=value)
+        print(f"Successfully wrote {value} to register {hex(register)}")
+
+    except modbus.ModbusError as e:
+        print(f"Modbus error: {e}")
+    except Exception as e:
+        print(f"Error: {e}")
+
+
+@connect(baud=9600)
+def read_correction(port, register, client=None, max_retries=20, retry_delay=0.09):
+    retries = 0
+    result = None
+
+    while retries < max_retries:
+        try:
+            # Read holding register (function code 03) from slave address 0x01
+            response = client.execute(0x01, cst.READ_HOLDING_REGISTERS, register, 1)
+            result = response[0]
+
+            # Validate the result, if it's valid, return it
+            if register == 0x021B:
+                if result is not None and 700 <= result <= 1000:
+                    return result
+            elif register == 0x021C and (result > 2000 or result < 50):
+                if result is not None:
+                    return result
+            else:
+                pass
+            # If result is invalid, log the attempt and retry
+            print(f"Invalid result: {result}. Retrying...")
+
+
+        except modbus.ModbusError as e:
+            print(f"Modbus error: {e}. Retrying...")
+            pass
+
+        except Exception as e:
+            print(f"Error: {e}. Retrying...")
+            pass
+
+        retries += 1
+        retry_delay += 0.1
+        time.sleep(retry_delay)
+
+    # If maximum retries are reached, return None or handle it accordingly
+    print("Maximum retries reached, unable to get valid response.")
+    return None
+
+
+@connect(baud=9600)
+def write_correction(port, register, value, client=None, max_retries=5, retry_delay=0.08):
+    retries = 0
+    success = False
+
+    while retries < max_retries and not success:
+        try:
+            # Write single holding register (function code 06) to slave address 0x01
+            client.execute(0x01, cst.WRITE_SINGLE_REGISTER, register, output_value=value)
+            # print(f"Successfully wrote {value} to register {hex(register)}")
+            success = True  # Set flag to true if write was successful
+
+        except modbus.ModbusError as e:
+            # print(f"Modbus error: {e}. Retrying...")
+            pass
+
+        except Exception as e:
+            # print(f"Error: {e}. Retrying...")
+            pass
+
+        retries += 1
+        retry_delay += 0.1
+        time.sleep(retry_delay)
+
+    if not success:
+        print(f"Failed to write {value} to register {hex(register)} after {max_retries} retries.")
+
+
+if __name__ == '__main__':
+    temp_port = "COM4"
+    set_port = "COM10"
+    read_temp(temp_port, 0x0100)
+    # time.sleep(0.08)
+    # write_correction(set_port, 0x021C, value=5)
+    write_correction(set_port, 0x021B, value=990)
+    time.sleep(0.08)
+    read_correction(set_port, 0x021B)
+    time.sleep(0.08)
+    read_correction(set_port, 0x021C)
+
+
+# @connect(baud=9600)
+# def read_correction(port, register, client=None, max_retries=5, retry_delay=0.1):
+#     try:
+#         # client = None
+#         # Read holding register (function code 03) from slave address 0x02
+#         response = client.execute(0x01, cst.READ_HOLDING_REGISTERS, register, 1)
+#         result = response[0]
+#         # print(f"Temperature: {temperature}°C")
+#         # print(f"result: {result}")
+#     except modbus.ModbusError as e:
+#         print(f"Modbus error: {e}")
+#         result = None
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         result = None
+#     return result
+
+
+# @connect(baud=9600)
+# def write_correction(port, register, value, client=None):
+#     try:
+#         # Write single holding register (function code 06) to slave address 0x01
+#         client.execute(0x01, cst.WRITE_SINGLE_REGISTER, register, output_value=value)
+#         print(f"Successfully wrote {value} to register {hex(register)}")
+#     except modbus.ModbusError as e:
+#         print(f"Modbus error: {e}")
+#     except Exception as e:
+#         print(f"Error: {e}")
